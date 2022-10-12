864f298c66201bc0deb116d5dd0df15a76695c81
chuchuluco <oscarfdez05@gmail.com>
2022-10-11 20:14:23 -0500

    Add insert and delete methods

--- lib/tree.rb
+++ lib/tree.rb
@@ -25,8 +25,43 @@ class Tree
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
+  def insert(value, node = @root)
+    return nil if value == node.data
+
+    if value < node.data
+      node.left.nil? ? node.left = Node.new(value) : insert(value, node.left)
+    else
+      node.right.nil? ? node.right = Node.new(value) : insert(value, node.right)
+    end
+  end
+
+  def delete(value, node = @root)
+    return node if node.nil?
+
+    if value < node.data
+      node.left = delete(value, node.left)
+    elsif value > node.data
+      node.right = delete(value, node.right)
+    else
+      return node.right if node.left.nil?
+      return node.left if node.right.nil?
+
+      leftmost_node = leftmost_leaf(node.right)
+      node.data = leftmost_node.data
+      node.right = delete(leftmost_node.data, node.right)
+    end
+    node
+  end
+
 end
 
 array_num =  [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 tree = Tree.new(array_num)
+tree.pretty_print
+array_num =  [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+tree = Tree.new(array_num)
+tree.pretty_print
+tree.insert(45)
+tree.pretty_print
+tree.delete(45)
 tree.pretty_print
